a01bcf1a202bf533c127cb6c44f38728bfac236c
Saviour <destydesty112@gmail.com>
2022-09-11 06:41:03 -0700

    printing using puts in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*This is a c program 
+*it prints a text using puts
+*it returns 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
