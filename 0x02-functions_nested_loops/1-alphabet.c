6c910f5ec0186abe6fa2d7800e34aba95160336b
Saviour <destydesty112@gmail.com>
2022-09-14 15:48:38 -0700

    ready for alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main: print alphabet
+ *
+ * Description: to print alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+	
+	for (c = 'a';  c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
