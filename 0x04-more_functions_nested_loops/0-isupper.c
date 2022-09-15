0bf5b662c0ac3c34d750197341eee3c6c4353e4a
Saviour <destydesty112@gmail.com>
2022-09-15 15:18:49 -0700

    check whether alphabet is capital or not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * isupper - check if a alphabet is upper or not
+ * @c: is the character or variables to test
+ * Return: 1 if true, otherwise 0 if false
+ */
+
+int _isupper(int c)
+{
+	if ((c >= "A") && (c <= "Z"))
+	{
+		return (1);
+	}
+
+	return (0);
+}
