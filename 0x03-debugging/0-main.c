f74da130baf8c79c71f3c74ef4f42d8cf4d0e295
Saviour <destydesty112@gmail.com>
2022-09-15 06:16:55 -0700

    Printing positive or negative integer

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test functions that prints integer positve or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
+
