9f35f5b385cb812222bfcbd4322a04ff1e3b5756
MrSavvy1 <destydesty117@hmail.com>
2022-12-08 13:54:17 -0800

    load

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-/*
+/**
  *This is a C Program 
- *This Functions  prints a text using puts
- *The Returns 0
+ *main:This Functions  prints a text using puts
+ *return: Renurns 0
  */
 
 int main(void)
 {
+	/*this function is for a loee*/
      puts("\"Programming is like building a multilingual puzzle");
+     /*this return is a null*/
      return (0);
 }
