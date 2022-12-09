0bd0d7fd2c37b537be7c4603bfc6d6132aa94d56
MrSavvy1 <destydesty117@hmail.com>
2022-12-08 20:12:24 -0800

    will up

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - the starting point, I think
- *   
- * Return: This is where I describe what the return does
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
