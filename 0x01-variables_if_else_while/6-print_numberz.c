6a7dbf83b64a5947d2e9c518e34bff34f1890493
MrSavvy1 <destydesty117@hmail.com>
2022-12-10 13:56:02 -0800

    convert char to int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Iliterate through the alphabet
+ *
+ * Return: It will return zero
+ */
+int main(void)
+{
+	int  alph;
+	/* some codes after declaration*/
+	for (alph = 0; alph < 10; alph++)
+	{
+		int putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
