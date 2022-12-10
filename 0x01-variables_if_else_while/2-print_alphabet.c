0a2f1f6180130c76d3ece35fabeb517d43b715a1
MrSavvy1 <destydesty117@hmail.com>
2022-12-10 08:52:23 -0800

    realhark

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Iliterate through the alphabet
+ *
+ * Return: It will return zero
+ */
+int main(void)
+{
+	char alph;
+	/* some codes after declaration*/
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+		putchar('\n');
+	return (0);
+}
