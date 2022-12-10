80029980beb85c97b53b698f7e5e1798482bf03a
MrSavvy1 <destydesty117@hmail.com>
2022-12-10 13:37:17 -0800

    int illiterate less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Iliterate through the alphabet
+ *
+ * Return: It will return zero
+ */
+int main(void)
+{
+	int alph;
+	/* some codes after declaration*/
+	for (alph = 0; alph < 10; alph++)
+	{
+		printf(alph);
+	}
+	putchar('\n');
+	return (0);
+}
