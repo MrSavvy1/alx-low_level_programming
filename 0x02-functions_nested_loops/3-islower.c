73a516ef20b6ffe12c244560fb48dca7072c57a6
MrSavvy1 <destydesty117@hmail.com>
2022-12-17 02:47:38 -0800

    loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for a lower case charactee
  * int c - is the parameter the islower uses
- * Return: This program will return a value according to the 
+ * Return: This program will return a value according to the
  */
 int _islower(int c)
 {
