5e80f2d87bc614431cd6c3c5b8bbdd3364a77feb
MrSavvy1 <destydesty117@hmail.com>
2022-12-19 20:32:31 -0800

    locale

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+	/* lios declare var */
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
