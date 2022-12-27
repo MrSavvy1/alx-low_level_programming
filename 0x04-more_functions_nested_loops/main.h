a795fb42c1e011ed4c0eaa714744273d02d4c7a7
MrSavvy1 <destydesty117@hmail.com>
2022-12-27 10:41:42 -0800

    lock up

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
-
 #include <stdio.h>
 
 /**
@@ -9,17 +8,18 @@
  * int prototypes (int)
  */
 
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void print_triangle(int size);
-void print_numbers(int n);
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+void print_triangle(int n);
+void print_number(int n);
+int pattern(int n);
 
 #endif /* MAIN_H */
