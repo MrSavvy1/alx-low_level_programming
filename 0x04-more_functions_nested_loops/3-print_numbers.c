d20bc21e84aec0d872c323e156f50f2b593be675
MrSavvy1 <destydesty117@hmail.com>
2022-12-27 23:38:00 -0800

    course

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,14 @@
 
 /**
  * print_numbers - print all numbers 0 through 9 and then new line
+ * return: always return 0
  */
 
 void print_numbers(void)
 {
 	int i;
+	/* declaration done */
 	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
-	_putchar('\n');
+	putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print every digit except 2 & 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+	/* declare done */
+	for (n = '0'; n <= '9'; n++)
+		if (n == '2' || n == '4')
+			continue;
+		_putchar(n);
+	_putchar('\n');
+}
