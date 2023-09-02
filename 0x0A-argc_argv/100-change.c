496fe40794f2f3298c083697a3955f12f6ff055e
MrSavvy1 <destydesty112@gmail.com>
2023-09-02 13:33:49 -0700

    value added

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,27 +1,38 @@
-#include <stdlib.h>
 #include <stdio.h>
-
-/*
- * removed as too many functions
- * greater25 - reduces n by as many 25 cent coins as possible
- * @n: cents
- * Return: remainder of cents after coins deducted
- *
- *int greater25(int n)
- *{
- *	int r = (n / 25) * 25;
- *
- *	return (n - r);
- *}
- */
+#include <stdlib.h>
 /**
- * greater10 - reduces n by as many 10 cent coins as possible
- * @n: cents
- * Return: remainder of cents after coins deducted
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int greater10(int n)
+int main(int argc, char *argv[])
 {
-	int r = (n / 10) * 10;
+	int cents, ncoins = 0;
 
-	return (n - r);
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,64 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-
+#include <ctype.h>
 /**
- * _strNumsOnly - checks a string to see if it only
- * contains digits
- * @s: a pointer to an array of strings
- * @n: the number of strings in the array
- * Return: 1 if only digits found, 0 otherwise
- */
-int _strNumsOnly(char *s)
-{
-	if (*s == '\0')
-		return (1);
-
-	if (!(*s >= '0' && *s <= '9'))
-		return (0);
-
-	return (_strNumsOnly(s + 1));
-}
-
-/**
- * main - adds positive numbers supplied as arguments to the program
- * @argc: counts the number of arguments supplied
- * @argv: makes an array of strings from the arguments supplied
- * Return: 0 for success, 1 for error
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int idx;
-	int numsOnly;
-	int total;
+	int i, j, add = 0;
 
-	/* if no or insufficient umbers are passed in */
-	if (argc <= 2)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	/* checking strings only contain digits */
-	idx = 1;
-	while (idx < argc)
-	{
-		numsOnly = _strNumsOnly(argv[idx]);
-		if (numsOnly == 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		idx++;
+		add += atoi(argv[i]);
 	}
-
-	idx = 1;
-	total = 0;
-	while (idx < argc)
-	{
-		total = total + atoi(argv[idx]);
-		idx++;
-	}
-	printf("%d\n", total);
+	printf("%d\n", add);
 	return (0);
 }
